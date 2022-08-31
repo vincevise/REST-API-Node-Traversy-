1b7416cb5bf219ae1c61244561e5721b2ceaa0dd
vincevise <vincentvuram204@gmail.com>
2022-08-31 15:49:55 +0530

    Initial REST API goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors')
 const port =  process.env.PORT || 5000;
-
+const connectDB = require('./config/db')
 const {errorHandler}  = require('./middleware/errorMiddleware')
+ 
+connectDB();
 
 const app = express();
 app.use(express.json())
